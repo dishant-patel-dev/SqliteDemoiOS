bbf3b3a22e24358d9688d4150f53c12ad2aeee00
iosfundamentals <dishantpatel.info@gmail.com>
2016-04-30 16:36:41 +0530

    Initial Commit

---
+++ SqliteDBDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SqliteDBDemo
+//
+//  Created by Dishant on 30/04/16.
+//  Copyright © 2016 dishant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SqliteDBDemoTests/SqliteDBDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  SqliteDBDemoTests.m
+//  SqliteDBDemoTests
+//
+//  Created by Dishant on 30/04/16.
+//  Copyright © 2016 dishant. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SqliteDBDemoTests : XCTestCase
+
+@end
+
+@implementation SqliteDBDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
